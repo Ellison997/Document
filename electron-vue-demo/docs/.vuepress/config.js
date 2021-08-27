6c48d70473eb4efab5424ba9f8bcff411b3178f3
ChunleTian <chuntian997@gmail.com>
2021-08-27 18:20:45 +0800

    优化侧边栏配置

--- electron-vue-demo/docs/.vuepress/config.js
+++ electron-vue-demo/docs/.vuepress/config.js
@@ -11,25 +11,41 @@ module.exports = {
     },
     themeConfig: {
         logo: '/assets/img/logo.jpg',
-        displayAllHeaders: true, // 默认值：false
+        displayAllHeaders: false, // 默认值：false
+        activeHeaderLinks: true, // 默认值：true
         nav: [
-            { text: 'Home', link: '/' },
+            { text: '首页', link: '/' },
+            { text: '文档', link: '/document/' },
             // { text: 'Github', link: 'https://github.com/Ellison997/Electron-Vue-Demo' },
         ],
-        sidebar: [
-            ['/introduction/', '简介'],
-            ['/sqlite3/', '集成 sqlite3'],
-            ['/serialport/', '集成 serialport']
-        ],
+        sidebar: {
+            '/document/': [{
+                title: '模块',
+                collapsable: false,
+                children: [
+                    '',
+                    'sqlite3',
+                    'serialport',
+                ]
+            }, {
+                title: '疑难解答',
+                collapsable: false,
+                children: [
+                    'sqlite3',
+                    'serialport',
+                ]
+            }]
+        },
         // 假定是 GitHub. 同时也可以是一个完整的 GitLab URL
         repo: 'Ellison997/Electron-Vue-Demo',
         // 自定义仓库链接文字。默认从 `themeConfig.repo` 中自动推断为
         // "GitHub"/"GitLab"/"Bitbucket" 其中之一，或是 "Source"。
         repoLabel: 'Github',
+
         // 假如你的文档仓库和项目本身不在一个仓库：
         docsRepo: 'Ellison997/Document',
         // 假如文档不是放在仓库的根目录下：
-        docsDir: 'electron-vue-demo',
+        docsDir: 'electron-vue-demo/docs',
         // 假如文档放在一个特定的分支下：
         docsBranch: 'main',
         // 默认是 false, 设置为 true 来启用
--- electron-vue-demo/docs/README.md
+++ electron-vue-demo/docs/README.md
@@ -12,5 +12,5 @@ features:
   details: 享受 Electron + Vue 的开发体验，使用Vue编写视图，同时可以使用 Electron 提供的多种原生API。
 - title: 多平台构建
   details: 丰富的多平台构建配置，简单修改即可为目标平台轻松构建。
-footer: MIT Licensed | Copyright © 2021-杭州湘亭科技有限公司
+footer: MIT Licensed | Copyright © 2021-杭州XX科技有限公司
 ---
