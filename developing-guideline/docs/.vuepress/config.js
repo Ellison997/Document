6d0e861d1f13322d393861600bf01c3fcdc885df
ChunleTian <chuntian997@gmail.com>
2021-11-02 17:55:30 +0800

    添加文档分类，修改配置

--- developing-guideline/docs/.vuepress/config.js
+++ developing-guideline/docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    title: 'Developing-Guideline',
-    description: 'Developing-Guideline 使用手册',
-    base: '/Developing-Guideline/',
+    title: '湘亭科技软件开发指南',
+    description: 'Developing-Guideline 手册',
+    base: '/Document/',
     configureWebpack: {
         resolve: {
             alias: {
@@ -10,13 +10,20 @@ module.exports = {
         }
     },
     themeConfig: {
-        logo: '/assets/img/logo.jpg',
+        logo: '/assets/img/logo.png',
         displayAllHeaders: false, // 默认值：false
         activeHeaderLinks: true, // 默认值：true
         nav: [
             { text: '首页', link: '/' },
-            { text: '文档', link: '/document/' },
-            // { text: 'Github', link: 'https://github.com/Ellison997/Developing-Guideline' },
+            { text: '编程规约', link: '/specification/' },
+            { text: 'Vue', link: '/vue/' },
+            { text: 'Node.js', link: '/nodejs/' },
+            { text: 'Android', link: '/android/' },
+            { text: 'Electron', link: '/electron/' },
+            { text: 'Git', link: '/git/' },
+            { text: '移动端', link: '/mobile/' },
+            { text: '服务器', link: '/server/' },
+            // { text: 'Github', link: 'https://github.com/Ellison997/Document/tree/main/developing-guideline' },
         ],
         sidebar: {
             '/document/': ['', {
@@ -36,7 +43,7 @@ module.exports = {
             }]
         },
         // 假定是 GitHub. 同时也可以是一个完整的 GitLab URL
-        repo: 'Ellison997/Developing-Guideline',
+        repo: 'Ellison997/Document',
         // 自定义仓库链接文字。默认从 `themeConfig.repo` 中自动推断为
         // "GitHub"/"GitLab"/"Bitbucket" 其中之一，或是 "Source"。
         repoLabel: 'Github',
@@ -44,7 +51,7 @@ module.exports = {
         // 假如你的文档仓库和项目本身不在一个仓库：
         docsRepo: 'Ellison997/Document',
         // 假如文档不是放在仓库的根目录下：
-        docsDir: 'electron-vue-demo/docs',
+        docsDir: 'developing-guideline/docs',
         // 假如文档放在一个特定的分支下：
         docsBranch: 'main',
         // 默认是 false, 设置为 true 来启用
--- developing-guideline/docs/README.md
+++ developing-guideline/docs/README.md
@@ -1,16 +1,16 @@
 ---
 home: true
-heroImage: /assets/img/logo.jpg
-heroText: Developing-Guideline
-tagline: Developing-Guideline 快速上手
+heroImage: /assets/img/logo.png
+heroText: 湘亭科技软件开发指南
+tagline: 你的代码应该是给下一个开发者的情书，而不是战书……
 actionText: 快速上手 →
 actionLink: /document/
 features:
-- title: 开箱即用
-  details: 集成 sqlite3、serialport、electron-update、axios、echarts等常用模块，在此基础上开发可快速实现需求。
-- title: Electron + Vue 驱动
-  details: 享受 Electron + Vue 的开发体验，使用Vue编写视图，同时可以使用 Electron 提供的多种原生API。
-- title: 多平台构建
-  details: 丰富的多平台构建配置，简单修改即可为目标平台轻松构建。
-footer: MIT Licensed | Copyright © 2021-杭州XX科技有限公司
+- title: 编程规约
+  details: 为实现代码一致性和最佳实践，通过代码风格的一致性，降低维护代码的成本以及改善多人协作的效率。
+- title: 技术指南
+  details: 遵守最佳实践，确保程序性能得到最佳优化与实现高效的代码。
+- title: 持续更新
+  details: 由内部开发者根据技术的持续迭代更新此文档，保证团队所有成员掌握最新的开发方式。
+footer: MIT Licensed | Copyright © 2021-杭州湘亭科技有限公司
 ---
